762e829baf021ed6bae2f7a0cabcedd729bc1503
CEOSiva <ceosivaofficial@gmail.com>
2026-01-03 19:30:29 +0530

    after firebase secreat fixed

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-const rawPrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+const rawPrivateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 // Convert escaped \n sequences in the private key into real newlines
 const privateKey = rawPrivateKey ? rawPrivateKey.replace(/\\n/g, "\n") : undefined;
